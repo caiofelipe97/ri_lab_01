c8184520d7a3b1b4d0a068c9dd5c005df91cd131
Caio Felipe <caio.felipe@ext.tfgco.com>
2019-04-08 17:21:38 -0300

    Finish

--- ri_lab_01/spiders/brasil_elpais.py
+++ ri_lab_01/spiders/brasil_elpais.py
@@ -4,12 +4,13 @@ import json
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
+from datetime import datetime
 
 
 class BrasilElpaisSpider(scrapy.Spider):
     name = 'brasil_elpais'
     allowed_domains = ['brasil.elpais.com']
-    start_urls = []]
+    start_urls = []
     data_limit = datetime.strptime('01/01/2018', '%d/%m/%Y')
 
 
@@ -21,14 +22,26 @@ class BrasilElpaisSpider(scrapy.Spider):
 
     def parse(self, response):
         for notice in response.css("div.articulo__envoltorio"):
-            yield{
-                "title": notice.css("h1.articulo-titulo::text").get(),
-                "subtitle": notice.css("h2.articulo-subtitulo::text").get(),
-                "author": notice.css("span.autor-nombre a::text").get()
-            }
+            texts = response.css('div.articulo__contenedor p::text').getall()
+            notice_text = ''
+            for text in texts:
+                notice_text += text.replace("\n", "")
+
+            data = notice.css('time::attr(datetime)').get()
+            editedData = datetime.strptime(data[:-6], '%Y-%m-%dT%H:%M:%S')
+            if(editedData >= self.data_limit):
+                yield{
+                    "title": notice.css("h1.articulo-titulo ::text").get(),
+                    "subtitle": notice.css("h2.articulo-subtitulo::text").get(),
+                    "author": notice.css("span.autor-nombre a::text").get(),
+                    "section": notice.css("div.seccion a.enlace span::text").get(),
+                    "data":   editedData,
+                    "text":   notice_text,
+                    "url":    response.url
+                }
         for notice_url in response.css("figure.foto a::attr(href)").getall():
             if notice_url is not None:
-                    yield scrapy.Request(notice_url, callback=self.parse)
+                yield scrapy.Request("https:" + notice_url, callback=self.parse)
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
